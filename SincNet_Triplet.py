c02b7f3532020a7ae7b39de45050434f08b4337c
kumbachamushroom <lucvanwyk@gmail.com>
2020-08-25 10:40:46 +0200

    Initial

---
+++ SincNet_Triplet.py
@@ -0,0 +1,147 @@
+
+from Triplet_Net import *
+from Triplet_DataLoader import Triplet_Time_Loader
+from SincNet_dataio import ReadList, read_conf,str_to_bool
+import wandb
+import torch.utils.data as data
+import torch.optim as optim
+import time
+from Metric_Losses import batch_hard_triplet_loss
+
+class AverageMeter:
+    '''Computes and stores the average and current value'''
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.valu = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val *n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def train(train_loader, SincNet_model, MLP_model, optimizer_SincNet, optimizer_MLP, epoch):
+    MLP_model.train()
+    SincNet_model.train()
+    losses = AverageMeter()
+    i = 0
+    for batch in iter(train_loader):
+        i =+ 1
+        tracks, int_labels, string_labels = batch
+        tracks, int_labels = tracks.cuda(), int_labels.cuda()
+        embeddings = SincNet_model(tracks)
+        embeddings = MLP_model(embeddings)
+        loss, correct_negative, total = batch_hard_triplet_loss(int_labels, embeddings, margin_positive=2,
+                                                                margin_negative=2, device='cuda',
+                                                                squared=True)
+        print("The loss is ", loss)
+        optimizer_SincNet.zero_grad()
+        #optimizer_MLP.zero_grad()
+        loss.backward()
+        optimizer_SincNet.step()
+        #optimizer_MLP.step()
+        losses.update(loss, 1)
+        print(' Train Epoch {} [{}/{}] \t Loss {:.4f} \t '.format(epoch, i, len(iter(train_loader)), losses.avg))
+
+
+
+
+
+def main():
+    #READ CONFIG FILE
+    options = read_conf()
+
+    #LOG ON WANDB?
+    log = options.wandb
+    project_name = options.project
+
+    if log:
+        wandb.init(project='SincNet_MetricLoss')
+        wandb.run.name = project_name
+
+    device = torch.device("cuda:0")
+
+    kwargs = {'num_workers' : 2, 'pin_memory':True}
+
+    #Get data path
+    data_PATH = options.path
+    train_loader = data.DataLoader(Triplet_Time_Loader(path=data_PATH, spectrogram=False, train=True), batch_size=4, shuffle=False, **kwargs)
+    test_loader = data.DataLoader(Triplet_Time_Loader(path=data_PATH, spectrogram=False, train=False), batch_size=4, shuffle=False, **kwargs)
+
+    #get parameters for SincNet and MLP
+    #[cnn]
+    # [cnn]
+    cnn_N_filt = list(map(int, options.cnn_N_filt.split(',')))
+    cnn_len_filt = list(map(int, options.cnn_len_filt.split(',')))
+    cnn_max_pool_len = list(map(int, options.cnn_max_pool_len.split(',')))
+    cnn_use_laynorm_inp = str_to_bool(options.cnn_use_laynorm_inp)
+    cnn_use_batchnorm_inp = str_to_bool(options.cnn_use_batchnorm_inp)
+    cnn_use_laynorm = list(map(str_to_bool, options.cnn_use_laynorm.split(',')))
+    cnn_use_batchnorm = list(map(str_to_bool, options.cnn_use_batchnorm.split(',')))
+    cnn_act = list(map(str, options.cnn_act.split(',')))
+    cnn_drop = list(map(float, options.cnn_drop.split(',')))
+
+    # [dnn]
+    fc_lay = list(map(int, options.fc_lay.split(',')))
+    fc_drop = list(map(float, options.fc_drop.split(',')))
+    fc_use_laynorm_inp = str_to_bool(options.fc_use_laynorm_inp)
+    fc_use_batchnorm_inp = str_to_bool(options.fc_use_batchnorm_inp)
+    fc_use_batchnorm = list(map(str_to_bool, options.fc_use_batchnorm.split(',')))
+    fc_use_laynorm = list(map(str_to_bool, options.fc_use_laynorm.split(',')))
+    fc_act = list(map(str, options.fc_act.split(',')))
+
+    # [optimization]
+    lr = float(options.lr)
+    batch_size = int(options.batch_size)
+    N_epochs = int(options.N_epochs)
+    N_batches = int(options.N_batches)
+    N_eval_epoch = int(options.N_eval_epoch)
+    seed = int(options.seed)
+    torch.manual_seed(1234)
+
+    SincNet_args = {'input_dim': 48000, #3 seconds at 16000Hz
+                   'fs': 16000,
+                   'cnn_N_filt': cnn_N_filt,
+                   'cnn_len_filt': cnn_len_filt,
+                   'cnn_max_pool_len': cnn_max_pool_len,
+                   'cnn_use_laynorm_inp': cnn_use_laynorm_inp,
+                   'cnn_use_batchnorm_inp': cnn_use_batchnorm_inp,
+                   'cnn_use_laynorm': cnn_use_laynorm,
+                   'cnn_use_batchnorm': cnn_use_batchnorm,
+                   'cnn_act': cnn_act,
+                   'cnn_drop': cnn_drop
+                   }
+    SincNet_model = SincNet(SincNet_args)
+    SincNet_model.to(device)
+
+    DNN1_args = {'input_dim': SincNet_model.out_dim,
+                 'fc_lay': fc_lay,
+                 'fc_drop': fc_drop,
+                 'fc_use_batchnorm': fc_use_batchnorm,
+                 'fc_use_laynorm': fc_use_laynorm,
+                 'fc_use_laynorm_inp': fc_use_laynorm_inp,
+                 'fc_use_batchnorm_inp': fc_use_batchnorm_inp,
+                 'fc_act': fc_act}
+
+    MLP_net = MLP(DNN1_args)
+    MLP_net.to(device)
+
+    optimizer_SincNet = optim.RMSprop(params=list(SincNet_model.parameters()) + list(MLP_net.parameters()), lr=lr, alpha=0.8, momentum=0.5)
+    optimizer_MLP = optim.RMSprop(params=MLP_net.parameters(), lr=lr, alpha=0.8, momentum=0.5)
+
+    for epoch in range(1, N_epochs+1):
+        start_time = time.time()
+        train(epoch=epoch, train_loader=train_loader, SincNet_model=SincNet_model, MLP_model=MLP_net, optimizer_SincNet=optimizer_SincNet, optimizer_MLP=optimizer_MLP)
+        duration = time.time() - start_time
+        print("Done training epoch {} in {:.4f}".format(epoch, duration))
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ SincNet_dataio.py
@@ -0,0 +1,90 @@
+import configparser as ConfigParser
+from optparse import OptionParser
+import numpy as np
+# import scipy.io.wavfile
+import torch
+
+
+def ReadList(list_file):
+    f = open(list_file, "r")
+    lines = f.readlines()
+    list_sig = []
+    for x in lines:
+        list_sig.append(x.rstrip())
+    f.close()
+    return list_sig
+
+
+def read_conf():
+    parser = OptionParser()
+    parser.add_option("--cfg")  # Mandatory
+    (options, args) = parser.parse_args()
+
+    cfg_file = options.cfg
+    Config = ConfigParser.ConfigParser()
+    Config.read(cfg_file)
+
+    # [data]
+    options.path = Config.get('data','path')
+
+    # [windowing]
+    options.fs = Config.get('windowing', 'fs')
+    options.cw_len = Config.get('windowing', 'cw_len')
+    options.cw_shift = Config.get('windowing', 'cw_shift')
+
+
+    #[wandb]
+    options.wandb = Config.getboolean('wandb','log')
+    options.project = Config.get('wandb','project')
+
+    # [cnn]
+    options.cnn_N_filt = Config.get('cnn', 'cnn_N_filt')
+    options.cnn_len_filt = Config.get('cnn', 'cnn_len_filt')
+    options.cnn_max_pool_len = Config.get('cnn', 'cnn_max_pool_len')
+    options.cnn_use_laynorm_inp = Config.get('cnn', 'cnn_use_laynorm_inp')
+    options.cnn_use_batchnorm_inp = Config.get('cnn', 'cnn_use_batchnorm_inp')
+    options.cnn_use_laynorm = Config.get('cnn', 'cnn_use_laynorm')
+    options.cnn_use_batchnorm = Config.get('cnn', 'cnn_use_batchnorm')
+    options.cnn_act = Config.get('cnn', 'cnn_act')
+    options.cnn_drop = Config.get('cnn', 'cnn_drop')
+
+    # [dnn]
+    options.fc_lay = Config.get('dnn', 'fc_lay')
+    options.fc_drop = Config.get('dnn', 'fc_drop')
+    options.fc_use_laynorm_inp = Config.get('dnn', 'fc_use_laynorm_inp')
+    options.fc_use_batchnorm_inp = Config.get('dnn', 'fc_use_batchnorm_inp')
+    options.fc_use_batchnorm = Config.get('dnn', 'fc_use_batchnorm')
+    options.fc_use_laynorm = Config.get('dnn', 'fc_use_laynorm')
+    options.fc_act = Config.get('dnn', 'fc_act')
+
+    # [class]
+    options.class_lay = Config.get('class', 'class_lay')
+    options.class_drop = Config.get('class', 'class_drop')
+    options.class_use_laynorm_inp = Config.get('class', 'class_use_laynorm_inp')
+    options.class_use_batchnorm_inp = Config.get('class', 'class_use_batchnorm_inp')
+    options.class_use_batchnorm = Config.get('class', 'class_use_batchnorm')
+    options.class_use_laynorm = Config.get('class', 'class_use_laynorm')
+    options.class_act = Config.get('class', 'class_act')
+
+    # [optimization]
+    options.lr = Config.get('optimization', 'lr')
+    options.batch_size = Config.get('optimization', 'batch_size')
+    options.N_epochs = Config.get('optimization', 'N_epochs')
+    options.N_batches = Config.get('optimization', 'N_batches')
+    options.N_eval_epoch = Config.get('optimization', 'N_eval_epoch')
+    options.seed = Config.get('optimization', 'seed')
+    #options.AMSoftmax = Config.get('optimization', 'AMSoftmax')
+    #options.AMSoftmax_m = Config.get('optimization', 'AMSoftmax_m')
+
+    return options
+
+
+def str_to_bool(s):
+    if s == 'True':
+        return True
+    elif s == 'False':
+        return False
+    else:
+        raise ValueError
+
+
