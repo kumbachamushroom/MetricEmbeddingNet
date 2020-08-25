e7ee362d65f994e9bc712ccdf99cc7009a4696d8
kumbachamushroom <lucvanwyk@gmail.com>
2020-08-25 10:45:19 +0200

    added scripts

---
+++ Compare_Embeddings.py
@@ -0,0 +1,244 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+from Triplet_DataLoader import TripletLoader
+from utlis import get_conv2d_output_shape, get_conv1d_output_shape
+import os
+import numpy as np
+import wandb
+from Triplet_Net import VGGVox, TripletNet
+import argparse
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.nn.functional as F
+from visdom import Visdom
+from torch.autograd import Variable
+from Triplet_DataLoader import Selective_Loader
+from losses import batch_hard_triplet_loss
+import numpy as np
+import time
+from Prepare_Track import Prepare_Track
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch.backends.cudnn as cudnn
+import torchaudio
+import random
+from Triplet_DataLoader import Triplet_Time_Loader
+
+#def compare_triplets_by_dist(negative_loader,positive_loader,margin,model):
+
+
+def Plot_Results(df_true,df_model):
+    speaker_labels = df_true.index.values
+    for speaker in speaker_labels:
+        fig,ax  = plt.subplots()
+        labels = df_true.loc[speaker,:].values[0:1000]
+        timestamps = np.where(labels == 1)
+        labels = labels[labels == 1]
+        plt.plot(timestamps[0], labels, 'o', label=speaker+'_truth')
+
+        labels = df_model.loc[speaker,:].values[0:1000]
+        timestamps = np.where(labels == 1)
+        labels = labels[labels == 1]
+        plt.plot(timestamps[0], labels*2, 'o', color='red', label=speaker+'_model')
+        plt.legend('ground truth', ' model ')
+        plt.show()
+
+
+
+
+
+
+def get_target_embeddings(num_embeddings, label_df, sample_list, model):
+    model.eval()
+    speaker_labels = label_df.index.values
+    f = [(line.split()[0], line.split()[1], line.split()[2], line.split()[3], line.split()[4]) for line in open(sample_list)]
+    Avg_Embedding_List = []
+    for speaker in speaker_labels:
+        print('Finding ',speaker)
+        samples = [sample for sample in f if sample[1] == speaker]
+        samples = random.sample(samples,num_embeddings)
+        embeddingList = []
+        for file in samples:
+            track, sample_rate = torchaudio.load(file[0])
+            track = track[0][(int(file[3])*sample_rate):(int(file[4])*sample_rate)]
+            track = track.view(1,-1)
+            spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400, hop_length=100)(track).view(1,1,201,481).cuda()
+            embedding = model(spectrogram)
+            embeddingList.append(embedding)
+        embeddingList = torch.stack(embeddingList).view(len(embeddingList),1,256)
+        mean = torch.mean(embeddingList, dim=0, keepdim=True)
+        Avg_Embedding_List.append((mean, speaker))
+    return Avg_Embedding_List
+
+def diarize(avg_embeddings, frame_list, label_df, model, path, margin=20):
+    model.eval()
+    track, sample_rate = torchaudio.load(path)
+    model_labels = pd.DataFrame(index=label_df.index.values, columns = label_df.columns.values)
+    for frame in label_df.columns.values:
+        start = int(frame_list[int(frame)][0])
+        stop = int(frame_list[int(frame)][1])
+        snippet = track[0][(start*sample_rate):(stop*sample_rate)].view(1,-1)
+        spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400, hop_length=100)(snippet).view(1,1,201,481).cuda()
+        embedding = model(spectrogram)
+        for speaker in avg_embeddings:
+            if torch.dist(speaker[0], embedding) < margin:
+                model_labels.loc[speaker[1],frame] = 1
+        print(" Scoring frame {}/{} ".format(frame, len(frame_list)), end='\r', flush=True)
+    model_labels.fillna(0)
+    return model_labels
+
+
+
+
+
+
+
+
+
+
+def get_average_embedding(window_size, num_embeddings, path, model):
+    track, sample_rate = torchaudio.load(path)
+    window_size = sample_rate*window_size
+    sampler_start = random.randint(0,len(track[0])-num_embeddings*window_size)
+    embeddingList = []
+    for i in range(num_embeddings):
+        sample = track[0][sampler_start:(sampler_start+window_size)]
+        sample = sample.view(1,-1)
+        spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400, hop_length=100)(sample)
+        spectrogram = spectrogram.cuda()
+        #print(spectrogram.size())
+        spectrogram = spectrogram.view(1,1,201,481)
+        sampler_start = sampler_start+window_size
+        embedding = model(spectrogram)
+        embeddingList.append(embedding)
+    #print(embedding.size())
+    #print(embeddingList)
+    embeddingList = torch.stack(embeddingList).view(len(embeddingList),1,256)
+    #print(embeddingList.size())
+    mean = torch.mean(embeddingList, dim=0,keepdim=True)
+    #mean = embeddingList[0]
+    return mean
+
+def compare_embeddings(frame_list,label_df,model,path,speaker_label,anchor, margin=0.45):
+    model.eval()
+    track, sample_rate = torchaudio.load(path)
+    frame_scores = np.zeros_like(label_df.loc[speaker_label,:].values)
+    true_labels = label_df.loc[speaker_label,:].values
+    for i, frame in enumerate(frame_list):
+        start = int(frame[0])
+        stop = int(frame[1])
+        snippet = track[0][(start*sample_rate):(stop*sample_rate)]
+        snippet = snippet.view(1,-1)
+        spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400,hop_length=100)(snippet)
+        spectrogram = spectrogram.cuda()
+        spectrogram = spectrogram.view(1,1,201,481)
+        embedding = model(spectrogram)
+        #if true_labels[i] == 1:
+        #    print(torch.dist(anchor,embedding,p=2))
+        frame_scores[i] = 1 if torch.dist(anchor,embedding,p=2) < 29 else 0
+        print(" Scoring frame {}/{} ".format(i,len(frame_list)), end='\r', flush=True)
+    result = frame_scores*label_df.loc[speaker_label,:].values
+    print(result)
+    accuracy = (np.count_nonzero(result)/np.count_nonzero(label_df.loc[speaker_label,:].values))*100
+    print(accuracy)
+
+    print('done')
+    return result
+
+def compare_track(model):
+    model.eval()
+    track1 = '/home/lucas/PycharmProjects/Papers_with_code/data/AMI/triplet_splits/EN2001a_Speaker_A_12.wav'
+    track2 = '/home/lucas/PycharmProjects/Papers_with_code/data/AMI/triplet_splits/EN2001a_Speaker_A_46.wav'
+    track3 = '/home/lucas/PycharmProjects/Papers_with_code/data/AMI/triplet_splits/EN2001a_Speaker_B_29.wav'
+    track1, sample_rate = torchaudio.load(track1)
+    track2, sample_rate = torchaudio.load(track2)
+    track3, sample_rate = torchaudio.load(track3)
+    spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400, hop_length=100)(track1)
+    spectrogram = spectrogram.view(1, 1, 201, 481)
+    embedding1 = model(spectrogram.cuda())
+    spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400, hop_length=100)(track2)
+    spectrogram = spectrogram.view(1, 1, 201, 481)
+    embedding2 = model(spectrogram.cuda())
+    spectrogram = torchaudio.transforms.Spectrogram(normalized=True, power=1, n_fft=400, hop_length=100)(track3)
+    spectrogram = spectrogram.view(1, 1, 201, 481)
+    embedding3 = model(spectrogram.cuda())
+    print(torch.dist(embedding1, embedding2, p=2))
+    print(torch.dist(embedding1,embedding3,p=2))
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    #Train settings
+    wandb.init(project="vgg_triplet")
+    global args, best_acc
+    parser = argparse.ArgumentParser(description='VGG Triplet-Loss Speaker Embedding')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                        help='input batch size for training')
+    parser.add_argument('--no-cuda', action='store_true',default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed')
+    parser.add_argument('--log-interval',type=int, default=20, metavar='N',
+                        help='how many batches to wait before logging training score')
+    parser.add_argument('--margin', type=float, default=1, metavar='M',
+                        help='margin for triplet loss (default: 0.2)')
+    parser.add_argument('--base-path',type=str,
+                        default='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/triplet_splits',
+                        help='string to triplets')
+    parser.add_argument('--s-file',default='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/amicorpus_individual/Extracted_Speech/trimmed_sample_list.txt',type=str,
+                        help='name of sample list')
+    parser.add_argument('--load-path',default='/home/lucas/PycharmProjects/Papers_with_code/data/models/VGG_Triplet'
+                        ,type=str, help='path to save models to')
+    parser.add_argument('--name', default='Compare_Embeddings', type=str,
+                        help='name of experiment')
+    parser.add_argument('--base-sample', default='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/amicorpus_individual/Extracted_Speech/TS3005a_MTD018ID.wav', type=str,
+                        help='path to base sample')
+    parser.add_argument('--test-path', type=str, default='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/pyannote/amicorpus/TS3005a/audio/TS3005a.Mix-Headset.wav'
+                        ,help='path to audio file to be used in testing')
+
+    args = parser.parse_args()
+
+    #wandb.run.name = args.name
+    #wandb.run.save()
+    #wandb.config.update(args)
+
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    print(args.no_cuda, torch.cuda.is_available())
+    if args.cuda:
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+    torch.manual_seed(args.seed)
+    kwargs = {'num_workers': 4, 'pin_memory': True} if args.cuda else {}
+
+    ground_truth = Prepare_Track(track_name='TS3005a.Mix-Headset',rttm_name='MixHeadset.train.rttm',path_to_track='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/pyannote/amicorpus/TS3005a/audio', path_to_rttm='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/pyannote/AMI')
+    true_label_df, true_frame_list, speaker_dict = ground_truth.label_frames(window_size=3, step_size=0.1)
+
+    model = VGGVox()
+    model.to(device)
+    model.load_state_dict(torch.load(args.load_path))
+    print("Model loaded from state dict")
+    cudnn.benchmark = True
+
+    #print(get_target_embeddings(num_embeddings=3,label_df=true_label_df,sample_list=args.s_file,model=model))
+    anchor = get_average_embedding(window_size=3, num_embeddings=10, path=args.base_sample, model=model)
+    #print(diarize(get_target_embeddings(num_embeddings=3, label_df=true_label_df, sample_list=args.s_file, model=model), frame_list=true_frame_list,label_df=true_label_df, model=model,path=args.test_path,margin=20))
+
+    speaker_label = args.base_sample[args.base_sample.rfind('_')+1:args.base_sample.rfind('.')]
+    result = compare_embeddings(frame_list=true_frame_list, label_df=true_label_df, model=model, path=args.test_path,anchor=anchor,speaker_label=speaker_label,
+                       margin=0.45)
+    #compare_track(model)
+    #Plot_Results(true_label_df, df_model=diarize(get_target_embeddings(num_embeddings=1, label_df=true_label_df, sample_list=args.s_file, model=model), frame_list=true_frame_list,label_df=true_label_df, model=model,path=args.test_path,margin=20))
+
+if __name__ == '__main__':
+    main()
---
+++ Triplet_Main.py
@@ -0,0 +1,331 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+from Triplet_DataLoader import TripletLoader
+from utlis import get_conv2d_output_shape, get_conv1d_output_shape
+import os
+import numpy as np
+import wandb
+from Triplet_Net import VGGVox, TripletNet
+import argparse
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.nn.functional as F
+from visdom import Visdom
+from torch.autograd import Variable
+from Triplet_DataLoader import Spectrogram_Loader
+from losses import batch_hard_triplet_loss
+import numpy as np
+from Triplet_DataLoader import Triplet_Time_Loader
+import time
+
+
+class AverageMeter:
+    '''Computes and stores the average and current value'''
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.valu = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val *n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(dista, distb):
+    margin = 0.2
+    pred = (distb - dista - margin).cpu().data
+    return (pred > 0).sum()*1.0/dista.size()[0]
+
+
+
+
+def test(test_loader, tnet, criterion, epoch):
+    losses = AverageMeter()
+    accs = AverageMeter()
+
+
+    # switch evaluation mode
+    tnet.eval()
+    for batch_idx, (data1, data2, data3) in enumerate(test_loader):
+        if args.cuda:
+            data1, data2, data3 = data1.cuda(), data2.cuda(), data3.cuda()
+        data1, data2, data3 = Variable(data1), Variable(data2), Variable(data3)
+
+        #compute output
+        dista, distb, embedding_x, embedding_y, embedding_z = tnet(data1,data2,data3)
+        target = torch.FloatTensor(dista.size()).fill_(1)
+        if args.cuda:
+            target = target.cuda()
+        target = Variable(target)
+        #test_loss = criterion(dista, distb, target).item()
+        test_loss = criterion(embedding_x, embedding_y, embedding_z).item()
+        #measure the accuracy and record the loss
+        acc = accuracy(dista, distb)
+        accs.update(acc, data1.size(0))
+        losses.update(test_loss,data1.size(0))
+
+    print('\nTest set: average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(
+            losses.avg, 100. * accs.avg))
+    #plotter.plot('acc', 'test', epoch, accs.avg)
+    #plotter.plot('loss','test', epoch, losses.avg)
+    wandb.log({"Test accuracy":accs.avg,"Test Loss":losses.avg})
+    return accs.avg
+
+def test_batch(test_loader, tnet, epoch):
+    tnet.eval()
+    losses = AverageMeter()
+
+    i = 0
+    total_samples = 0
+    correct_negative_samples = 0
+    correct_positive_samples = 0
+
+    with torch.no_grad():
+        for batch in iter(test_loader):
+            spectrograms, int_labels, string_labels = batch
+            int_labels = int_labels.cuda()
+            # labels = labels.view(labels.size(0), -1).cuda()
+            spectrograms = spectrograms.cuda()
+            embeddings = tnet(spectrograms)
+            loss, correct_positive, correct_negative, total,_ = batch_hard_triplet_loss(int_labels, embeddings, margin_negative=2,margin_positive=2, device='cuda', squared=True)
+            losses.update(loss,1)
+            correct_negative_samples = correct_negative_samples + correct_negative
+            correct_positive_samples = correct_positive_samples + correct_positive
+            total_samples = total_samples + (total*2)
+            #negative_dist = negative_dist + hardest_neg.sum()
+            #positive_dist = positive_dist + hardest_pos.sum()
+
+
+        print('Epoch {} Test Accuracy: {:.4f} Test_Loss: {:.4f} \t'.format(epoch, (
+            (correct_negative_samples + correct_positive_samples) / total_samples),losses.avg))
+        wandb.log({"Test Accuracy": (correct_negative_samples + correct_positive_samples) / total_samples, "Test Loss": losses.avg})
+        return
+
+
+def train_batch(train_loader, tnet, optimizer, epoch):
+    tnet.train()
+    losses = AverageMeter()
+    i = 0
+    total_samples = 0
+    correct_negative_samples = 0
+    correct_positive_samples = 0
+    for batch in iter(train_loader):
+        i = i + 1
+
+        spectrograms, int_labels, string_labels = batch
+        int_labels = int_labels.cuda()
+        #labels = labels.view(labels.size(0), -1).cuda()
+        spectrograms = spectrograms.cuda()
+        embeddings = tnet(spectrograms)
+
+        loss, correct_positive, correct_negative, total, _ = batch_hard_triplet_loss(int_labels, embeddings,
+                                                                                     margin_negative=2,
+                                                                                     margin_positive=2, device='cuda',
+                                                                                     squared=True)
+        correct_negative_samples = correct_negative_samples + correct_negative
+        correct_positive_samples = correct_positive_samples + correct_positive
+        total_samples = total_samples + (total * 2)
+        #negative_dist = negative_dist + hardest_neg.sum()
+        #positive_dist = positive_dist + hardest_pos.sum()
+        losses.update(loss,1)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print('Train Epoch: {} [{}/{}]\t'
+              'Loss: {:.4f} \t'
+              .format(
+            epoch, i, len(iter(train_loader)),
+            loss))
+    print('Epoch {} Accuracy: {:.4f} Loss: {:.4f} \t'.format(epoch,((correct_positive_samples+correct_negative_samples)/total_samples),losses.avg))
+    wandb.log({"Train Accuracy":(correct_positive_samples+correct_negative_samples)/total_samples, "Train Loss":losses.avg})
+
+
+
+
+def train(train_loader, tnet, criterion, optimizer, epoch):
+    losses = AverageMeter()
+    accs = AverageMeter()
+    emb_norms = AverageMeter()
+
+    #switch to train mode
+    tnet.train()
+
+    for batch_idx, (data1, data2, data3) in enumerate(train_loader):
+        if args.cuda:
+            data1, data2, data3 = data1.cuda(), data2.cuda(), data3.cuda()
+        data1, data2, data3 = Variable(data1), Variable(data2), Variable(data3)
+
+        #compute the output
+        dista, distb, embedded_x, embedded_y, embedded_z = tnet(data1, data2, data3)
+        # 1 means, dista should be larger than distb
+        target = torch.FloatTensor(dista.size()).fill_(1)
+        if args.cuda:
+            target = target.cuda()
+        target = Variable(target)
+
+        loss_triplet = criterion(embedded_x, embedded_y, embedded_z)
+        loss_embedd = embedded_x.norm(2) + embedded_y.norm(2) + embedded_z.norm(2)
+        #loss = loss_triplet + 0.0001 * loss_embedd
+        #print("THE LOSS TRIPLET IS: ",loss_triplet)
+        #measure accuracy and record loss
+        acc = accuracy(dista,distb)
+        losses.update(loss_triplet.item(), data1.size(0))
+        accs.update(acc, data1.size(0))
+        emb_norms.update(loss_embedd.item()/3, data1.size(0))
+
+        #compute gradient and do optimizer step
+        optimizer.zero_grad()
+        loss_triplet.backward()
+        optimizer.step()
+
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{}]\t'
+                  'Loss: {:.4f} ({:.4f}) \t'
+                  'Acc: {:.2f}% ({:.2f}%) \t'
+                  'Emb_Norm: {:.2f} ({:.2f})'.format(
+                epoch, batch_idx * len(data1), len(train_loader.dataset),
+                losses.val, losses.avg,
+                       100. * accs.val, 100. * accs.avg, emb_norms.val, emb_norms.avg), end='\r', flush=True)
+        # log avg values to somewhere
+        #plotter.plot('acc', 'train', epoch, accs.avg)
+        #plotter.plot('loss', 'train', epoch, losses.avg)
+        #plotter.plot('emb_norms', 'train', epoch, emb_norms.avg)
+        wandb.log({"Train accuracy": accs.avg, "Train loss":losses.avg, "Train Embedding Norms":emb_norms.avg})
+
+
+def main():
+    #Train settings
+    #wandb.init(project="vgg_triplet_modified")
+    wandb.init(project="vggvox_modified_euclidean_triplet")
+
+    global args, best_acc
+    parser = argparse.ArgumentParser(description='VGG Triplet-Loss Speaker Embedding')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                        help='input batch size for training')
+    parser.add_argument('--test-batch-size', type=int, default=32, metavar='N',
+                        help='input batch size for testing')
+    parser.add_argument('--epochs',type=int, default=50, metavar='N',
+                        help='number of epochs for training')
+    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                        help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum')
+    parser.add_argument('--no-cuda', action='store_true',default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=2, metavar='S',
+                        help='random seed')
+    parser.add_argument('--log-interval',type=int, default=20, metavar='N',
+                        help='how many batches to wait before logging training score')
+    parser.add_argument('--margin', type=float, default=2, metavar='M',
+                        help='margin for triplet loss (default: 0.2)')
+    parser.add_argument('--resume', default='', type=str,
+                        help='path to latest checkpoint (default: None)')
+    parser.add_argument('--name', default='TripletNet_RMSprop', type=str,
+                        help='name of experiment')
+    parser.add_argument('--base-path',type=str,
+                        default='/home/lucas/PycharmProjects/Papers_with_code/data/AMI/amicorpus_individual/Extracted_Speech',
+                        help='string to triplets')
+    parser.add_argument('--ap-file',default='anchor_pairs.txt',type=str,
+                        help='name of file with anchor-positive pairs')
+    parser.add_argument('--s-file',default='trimmed_sample_list.txt',type=str,
+                        help='name of sample list')
+    parser.add_argument('--save-path',default='/home/lucas/PycharmProjects/Papers_with_code/data/models/VGG_Triplet_Modified'
+                        ,type=str, help='path to save models to')
+    parser.add_argument('--save', type=bool, default=True,
+                        help='save model?')
+    parser.add_argument('--load', type=bool, default=True,
+                        help='load model from latest checkpoint')
+    args = parser.parse_args()
+
+    wandb.run.name = args.name
+    wandb.run.save()
+    wandb.config.update(args)
+
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    print(args.no_cuda)
+    print(torch.cuda.is_available())
+    if args.cuda:
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+    torch.manual_seed(args.seed)
+    kwargs = {'num_workers': 2, 'pin_memory': True} if args.cuda else {}
+
+    #train_loader = torch.utils.data.DataLoader(TripletLoader(base_path=args.base_path,anchor_positive_pairs=args.ap_file,sample_list=args.s_file,train=True),
+    #                                           batch_size=args.batch_size,shuffle=True,**kwargs)
+    #test_loader = torch.utils.data.DataLoader(TripletLoader(base_path=args.base_path,anchor_positive_pairs=args.ap_file,sample_list=args.s_file,train=False),
+    #                                          batch_size=args.test_batch_size,shuffle=True,**kwargs)
+
+    #single_train_loader = torch.utils.data.DataLoader(Spectrogram_Loader(base_path=args.base_path, anchor_positive_pairs=args.ap_file, sample_list=args.s_file, train=True), batch_size=args.batch_size, shuffle=True, **kwargs)
+    #single_test_loader = torch.utils.data.DataLoader(Spectrogram_Loader(base_path=args.base_path, anchor_positive_pairs=args.ap_file, sample_list=args.s_file, train=False), batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    train_time_loader = torch.utils.data.DataLoader(Triplet_Time_Loader(path=os.path.join(args.base_path,args.s_file), train=True, spectrogram=True), batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_time_loader = torch.utils.data.DataLoader(Triplet_Time_Loader(path=os.path.join(args.base_path,args.s_file), train=False, spectrogram=True), batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    #global plotter
+    #plotter = VisdomLinePlotter(env_name=args.name)
+
+
+    model = VGGVox()
+    if args.cuda:
+        model.to(device)
+    if args.load:
+        model.load_state_dict(torch.load(args.save_path))
+        print("Model loaded from state dict")
+    #tnet = TripletNet(model)
+    #if args.cuda:
+    #    tnet.to(device)
+    wandb.watch(model)
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+    #criterion = torch.nn.MarginRankingLoss(margin = args.margin)
+    criterion = nn.TripletMarginLoss(margin=args.margin, p=2)
+
+    #optimizer = optim.Adam(tnet.parameters(),lr=args.lr)
+    #optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.RMSprop(model.parameters(),lr=args.lr ,alpha=0.8, momentum=args.momentum)
+    
+    #n_parameters = sum([p.data.nelement() for p in tnet.parameters()])
+    #print('  + NUmber of params: {}'.format(n_parameters))
+
+    for epoch in range(1, args.epochs+1):
+        start_time = time.time()
+        train_batch(train_time_loader,model,optimizer,epoch)
+        test_batch(test_time_loader,model,epoch)
+        duration = time.time() - start_time
+        if (epoch % 10) == 0:
+            torch.save(model.state_dict(), args.save_path)
+            print("Model Saved")
+        print("Done training epoch {} in {:.4f}".format(epoch, duration))
+
+    #for epoch in range(1, args.epochs + 1):
+    #    test_batch(single_train_loader, model, epoch)
+
+    if args.save:
+        torch.save(model.state_dict(),args.save_path)
+        print("Model Saved")
+
+
+
+if __name__ == '__main__':
+    main()
